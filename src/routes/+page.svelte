4b61b64d237e3d99f1645d16650f94fc5b58503c
maha507 <132257569+maha507@users.noreply.github.com>
2023-08-29 11:10:56 +0530

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>Well Come.......</h1>
+<h1>Well Come.....</h1>
+<div>
+	<h1>click here <a href="/jsapps/settings/business-profile"><u>Business Profile</u></a> to open business-profile</h1>
+</div>
+<div>
+	<h1>click here <a href="/jsapps/settings/my-profile"><u>My Profile</u></a> to open my-profile</h1>
+</div>
